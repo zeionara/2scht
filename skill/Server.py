a5e3912f9d0dc78e007ccea2c8302fc472cf02f1
Dmitrii <zeionara@gmail.com>
2024-04-24 02:14:30 +0300

    feat(threads): added support for voice generation for threads

--- skill/Server.py
+++ skill/Server.py
@@ -1,13 +1,33 @@
 from flask import Flask, request, json
+from requests import get
+
+from much import Fetcher
+
+from .Thread import Thread
 
 
 DEFAULT_PORT = 1217
+CATALOG_URL = 'https://2ch.hk/b/catalog.json'
+N_THREADS_PER_RESPONSE = 5
 
 
 class Server:
     def __init__(self):
         self.app = Flask('2scht speech skill server')
+        self.fetcher = Fetcher()
+
         json.provider.DefaultJSONProvider.ensure_ascii = False
+
+        self._threads = None
+        self._offset = None
+
+    def list_threads(self, reverse: bool = True, skip_first_n: int = 75):
+        response = get(CATALOG_URL, timeout = 60)
+
+        if (status_code := response.status_code) != 200:
+            raise ValueError(f'Can\'t pull threads, response status code is {status_code}')
+
+        return sorted(Thread.from_list(response.json()['threads']), key = lambda thread: (thread.length, thread.freshness), reverse = reverse)[skip_first_n:]
 
     def serve(self, host: str = '0.0.0.0', port = DEFAULT_PORT):
         app = self.app
@@ -18,11 +38,90 @@ class Server:
 
             session = request_json.get('session')
             version = request_json.get('version')
-            original_utterance = request_json.get('request', {}).get('original_utterance')
+            original_utterance = request_json.get('request', {}).get('original_utterance').lower()
+
+            # print(self._offset)
+
+            if self._threads is None or 'хочу' in original_utterance:
+                self._threads = self.list_threads()
+                self._offset = 0
+            else:
+
+                # if 'альфа' in original_utterance:
+                #     self._offset = min(len(self._threads) - N_THREADS_PER_RESPONSE, self._offset + N_THREADS_PER_RESPONSE)
+
+                if 'стоп' in original_utterance:
+                    self._threads = None
+                    self._offset = None
+
+                    return {
+                        'response': {
+                            'text': 'Завершаю показ тредов',
+                            'end_session': True
+                        },
+                        'session': session,
+                        'version': version
+                    }
+                else:
+                    if 'первый' in original_utterance:
+                        target_item = self._offset
+                    elif 'второй' in original_utterance:
+                        target_item = self._offset + 1
+                    elif 'третий' in original_utterance:
+                        target_item = self._offset + 2
+                    elif 'четвертый' in original_utterance or 'четвёртый' in original_utterance:
+                        target_item = self._offset + 3
+                    elif 'пятый' in original_utterance:
+                        target_item = self._offset + 4
+                    else:
+                        target_item = None
+                        self._offset = min(len(self._threads) - N_THREADS_PER_RESPONSE, self._offset + N_THREADS_PER_RESPONSE)
+
+                    if target_item is not None:
+                        thread = [self._threads[target_item].title_text] + [
+                            comment
+                            for topic in self.fetcher.fetch(self._threads[target_item].link, verbose = True)
+                            for comment in topic.comments
+                        ]
+
+                        print(len(thread), thread)
+
+                        return {
+                            'response': {
+                                # 'text': f'Ты говоришь: "{original_utterance}", я тебе отвечаю: привет, мир',
+                                'commands': [
+                                    {
+                                        "type": "TTS",
+                                        "text": comment,
+                                        "tts": comment,
+                                        "voice": "vasilisa-hifigan" if i % 2 < 1 else 'pavel-hifigan'
+                                    }
+                                    for i, comment in enumerate(thread)
+                                ],
+                                'end_session': False
+                            },
+                            'session': session,
+                            'version': version
+                        }
+
+                    # print(self._threads[self._offset].link)
+                    # self._offset = max(0, self._offset - N_THREADS_PER_RESPONSE)
+
+            threads = self._threads
+            offset = self._offset
 
             return {
                 'response': {
-                    'text': f'Ты говоришь: "{original_utterance}", я тебе отвечаю: привет, мир',
+                    # 'text': f'Ты говоришь: "{original_utterance}", я тебе отвечаю: привет, мир',
+                    'commands': [
+                        {
+                            "type": "TTS",
+                            "text": threads[i].title_text,
+                            "tts": f'Тред номер {i + 1}. {threads[i].title_text}',
+                            "voice": "vasilisa-hifigan" if i % 2 < 1 else 'pavel-hifigan'
+                        }
+                        for i in range(offset, offset + N_THREADS_PER_RESPONSE)
+                    ],
                     'end_session': False
                 },
                 'session': session,
