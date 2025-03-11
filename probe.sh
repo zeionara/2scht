0fa01d6b431f3b4384e250456572e8bd880b6db1
Zeio Nara <zeionara@gmail.com>
2025-03-11 21:28:07 +0300

    feat(tunnel): added script for running an https tunnel

--- probe.sh
+++ probe.sh
@@ -2,7 +2,9 @@
 
 curl \
     -H 'Content-Type: application/json' \
-    -d '{"session": {"id": 17}, "version": "1.0"}' \
-    'https://4b81-77-234-196-15.ngrok-free.app'
+    -d '{"request": {"original_utterance": "foo"}, "session": {"id": 17}, "version": "1.0"}' \
+    'https://careful-poodle-proven.ngrok-free.app'
+    # 'http://localhost:1217/'
+    # 'https://4b81-77-234-196-15.ngrok-free.app'
     # 'http://localhost:1217'
     # --insecure \
---
+++ start-tunnel.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+while true; do
+    lt --port 1217 --subdomain zeio-nara-threads --print-requests
+done
